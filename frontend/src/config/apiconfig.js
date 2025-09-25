84f6098eeb59efb2a16fd5a777847df88dd726a1
himansh025 <himanshudeolia0825@gmail.com>
2025-09-25 18:04:13 +0530

    api integrate

--- frontend/src/config/apiconfig.js
+++ frontend/src/config/apiconfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const apiUrl = import.meta.env.VITE_API_URL;
-const apiUrl = 'http://localhost:5000/api';
+const apiUrl = import.meta.env.VITE_API_URL;
+// const apiUrl = 'http://localhost:5000/api';
 
 const axiosInstance = axios.create({
   baseURL: apiUrl,
